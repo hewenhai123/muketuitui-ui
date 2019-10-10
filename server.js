12817b68ad40b7457f98b2c6f26dd4743d301748
sea <1194630180@qq.com>
2019-10-10 14:52:32 +0800

    20190916

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express=require('express');
 const app=express();
 const path =require('path');
+app.use('/public',express.static('public'));
 app.get('/',function(req,res){
        console.log(req.route);
        console.log(req.query);
