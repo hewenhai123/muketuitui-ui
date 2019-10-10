ce2740e26afbd50e310fd9be3bbf53fba858feb3
sea <1194630180@qq.com>
2019-10-10 14:31:39 +0800

    20190916

--- server.js
+++ server.js
@@ -2,14 +2,14 @@ const express=require('express');
 const app=express();
 
 app.get('/',function(req,res){
-       console.log(req.parameters)
+       console.log(req);
     res.send("hello world");
 
-})
+});
 
 const server =app.listen(8080,()=>{
     let host=server.address().address
     let port=server.address().port
     console.log("服务器已经启动",host,port);
 
-})
+});
